e635d45ee3e058ce8abec34110da41161cea16d7
albertotl <770079@unizar.es>
2021-05-19 03:22:27 +0200

    Delete src/tresenraya directory

--- tresEnRaya/src/tresenraya/Ficha.java
+++ tresEnRaya/src/tresenraya/Ficha.java
@@ -3,19 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tresenraya;
+package Modelo;
 
 /**
  *
  * @author Alberto
  */
-public class TresEnRaya {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Ficha {
+    protected String tipo;
+   
+    public String devolverTipo(){
+        return tipo;
     }
-    
 }
