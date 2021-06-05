e914ea78d9a44987cf5c8a02d4755ad4e63b7f0a
albertotl <770079@unizar.es>
2021-06-05 17:10:03 +0200

    Delete src/tresenraya directory

--- src/Vista/VistaMenu/InicioVista.java
+++ src/Vista/VistaMenu/InicioVista.java
@@ -97,6 +97,11 @@ public class InicioVista extends JFrame implements PropertyChangeListener{
         });
 
         jButton2.setText("Desconectar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -147,6 +152,12 @@ public class InicioVista extends JFrame implements PropertyChangeListener{
         juegoVista.visualizarVentana();
         this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        InicioSesionVista inicioSesion = InicioSesionVista.instancia(oyenteVista, juego);
+        inicioSesion.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
